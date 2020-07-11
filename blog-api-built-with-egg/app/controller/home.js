a6710e5a9fc96d59041bca022407e7ce16b556a0
west <1134608353@qq.com>
2020-07-11 23:48:29 +0800

    feat: egg logger

--- blog-api-built-with-egg/app/controller/home.js
+++ blog-api-built-with-egg/app/controller/home.js
@@ -1,8 +1,30 @@
 const { Controller } = require('egg');
+const winston = require('winston');
+const path = require('path');
+
+const logger = winston.createLogger({
+  level: 'info',
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({
+      // filename: 'winston-logs/myown-test.log'
+      filename: path.resolve(__dirname, '../../logs/winston/my-test.log'),
+    }),
+  ],
+});
 
 class HomeCtl extends Controller {
   async index() {
     const { ctx } = this;
+    // logger.log({
+    //   level: 'debug',
+    //   message: 'somebody access homepage',
+    //   data: {
+    //     ua: ctx.request.headers['user-agent'],
+    //   }
+    // });
+    logger.info('somebody are accessing homepage now! haha');
+
     ctx.body = 'hello world';
   }
 }
--- blog-api-built-with-egg/config/config.default.js
+++ blog-api-built-with-egg/config/config.default.js
@@ -1,7 +1,15 @@
+const path = require('path');
+
 exports.keys = 'dfsak^33#J';
 
 exports.security = {
   csrf: {
     enable: false,
   },
+}
+
+exports.logger = {
+  dir: path.join(__dirname, '../logs/egg-logger'),
+  level: 'INFO',
+  consoleLevel: 'DEBUG',
 }
